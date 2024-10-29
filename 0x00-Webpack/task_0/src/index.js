fc294e714ffcaf7705942479bbbebb45291a02e7
bo1atito <shorungbebolatito@gmail.com>
2024-10-29 23:58:20 +0100

    no main file

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,7 +1,7 @@
 import $ from 'jquery'
 
 // if Doc is ready add a function that add these paragraphs
-$( function() {
+$(document).ready( function() {
     $('body').append('<p>Holberton Dashboard</p>')
     $('body').append('<p>Dashboard data for the students</p>')
     $('body').append('<p>Copyright - Holberton School</p>')
