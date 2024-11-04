f711f35ae31d90b977c6507666eb0484351bc438
bo1atito <shorungbebolatito@gmail.com>
2024-11-04 15:51:41 +0100

    react

---
+++ 0x01-react_intro/task_2/README.md
@@ -0,0 +1,1 @@
+# ALX's Introduction To React
---
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Notifications.css'
+import close from '../src/close-icon.png'
+import { getLatestNotifications } from './utils'
+
+const notification = getLatestNotifications();
+
+export function Notifications() {
+    return <div className='Notifications'>
+        <p>Here is the list of notifications</p>
+        <ul>
+            <li data-priority='default'>New course available</li>
+            <li data-priority='urgent'>New resume available</li>
+            <li dangerouslySetInnerHTML={{__html: notification}}/>
+        </ul>
+        <button aria-label='Close' 
+                style={{position: 'absolute', right: '0', top: '1vh', border: 'none'}}
+                onClick={() => {console.log('Close button has been clicked')}}
+                >
+                    <img src={close}/>
+                </button>
+    </div>
+
+}
